b657316ad651e25b09ba856937a04ab6d495e058
Eder Aparecido Batista <eder.batista@wowsolution.com>
2021-05-02 23:17:23 -0300

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_Nelio_Alves.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
